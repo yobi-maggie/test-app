7d59a1a00292d2115a5c33ce93b317a17b3d5164
King <yongzhi.ding@duyi-inc.com>
2021-03-08 21:13:03 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,48 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import defaultPage from '@/layout/default.vue'
+import blankPage from '@/layout/blank.vue'
+import goodsList from '@/page/goodsList.vue'
+import Index from '@/page/index.vue'
+import ChangeCity from '@/page/changeCity.vue'
+import Login from '@/page/login.vue'
+import Register from '@/page/register.vue'
+Vue.use(Router)
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'default',
+      component: defaultPage,
+      redirect: '/index',
+      children: [{
+        path: 's/:name',
+        name: 'goods',
+        component: goodsList
+      }, {
+        path: '/index',
+        name: 'index',
+        component: Index
+      }, {
+        path: '/changeCity',
+        name: 'changeCity',
+        component: ChangeCity
+      }]
+    }, {
+      path: '/blank',
+      name: 'blank',
+      component: blankPage,
+      children: [{
+        path: 'login',
+        name: 'login',
+        component: Login
+      }, {
+        path: 'register',
+        name: 'register',
+        component: Register
+      }]
+    }
+  ]
 })
-
-export default router
